a2243f1f6b23d36bc2fe78358857a0600683d140
Sergey Fomenko <fomenko.personal@gmail.com>
2018-12-22 21:37:52 +0300

    added mock data

---
+++ js/main.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var data = [
+  {
+    'author': {
+      'avatar': 'img/avatars/user01.png'
+    },
+
+    'offer': {
+      'type': 'palace'
+    },
+
+    'location': {
+      'x': 10,
+      'y': 130
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user02.png'
+    },
+
+    'offer': {
+      'type': 'flat'
+    },
+
+    'location': {
+      'x': 20,
+      'y': 150
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user03.png'
+    },
+
+    'offer': {
+      'type': 'house'
+    },
+
+    'location': {
+      'x': 30,
+      'y': 170
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user04.png'
+    },
+
+    'offer': {
+      'type': 'bungalo'
+    },
+
+    'location': {
+      'x': 40,
+      'y': 190
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user05.png'
+    },
+
+    'offer': {
+      'type': 'palace'
+    },
+
+    'location': {
+      'x': 50,
+      'y': 210
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user06.png'
+    },
+
+    'offer': {
+      'type': 'flat'
+    },
+
+    'location': {
+      'x': 60,
+      'y': 230
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user07.png'
+    },
+
+    'offer': {
+      'type': 'house'
+    },
+
+    'location': {
+      'x': 70,
+      'y': 250
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user08.png'
+    },
+
+    'offer': {
+      'type': 'bungalo'
+    },
+
+    'location': {
+      'x': 80,
+      'y': 270
+    }
+  }
+];
