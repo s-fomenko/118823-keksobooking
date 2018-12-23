459206b62c8a66d28f0e4cd093fbca9d91f98393
Sergey Fomenko <fomenko.personal@gmail.com>
2018-12-23 16:49:56 +0300

    created function createPin

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
+var mockData = [];
+
 var createMockData = function () {
-  var data = [];
   var type = ['palace', 'flat', 'house', 'bungalo'];
 
   for (var i = 1; i <= 8; i++) {
@@ -11,14 +12,37 @@ var createMockData = function () {
     dataItem.offer = {'type': type[Math.floor(Math.random() * type.length)]};
     dataItem.location = {'x': Math.floor(Math.random() * 1200), 'y': Math.floor(Math.random() * 131 + 500)};
 
-    data.push(dataItem);
+    mockData.push(dataItem);
   }
-  return data;
+  return mockData;
 };
 
+createMockData();
 
-console.log(createMockData());
+var pageMap = document.querySelector('.map');
+pageMap.classList.remove('map--faded');
 
-/* var pageMap = document.querySelector('.map');
-pageMap.classList.remove('map--faded');*/
+var mapArea = document.querySelector('.map__pins');
+var mapPin = document.querySelector('#pin')
+  .content
+  .querySelector('.map__pin');
 
+var fragment = document.createDocumentFragment();
+
+var createPin = function (data) {
+  for (var i = 0; i < 8; i++) {
+    var item = mapPin.cloneNode(true);
+    item.style.left = data[i].location.x + 'px';
+    item.style.right = data[i].location.y + 'px';
+    item.querySelector('img').src = data[i].author.avatar;
+    item.querySelector('img').alt = data[i].offer.type;
+    fragment.appendChild(item);
+  }
+  return fragment;
+};
+
+// createPin(mockData);
+console.log(createPin(mockData));
+
+
+// console.log(data[i].author.avatar);
