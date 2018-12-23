afb42c5d7881b50707f1456727d865ab12897ebb
Sergey Fomenko <fomenko.personal@gmail.com>
2018-12-23 14:41:59 +0300

    created createMockData function

--- js/main.js
+++ js/main.js
@@ -1,121 +1,24 @@
 'use strict';
 
-var data = [
-  {
-    'author': {
-      'avatar': 'img/avatars/user01.png'
-    },
+var createMockData = function () {
+  var data = [];
+  var type = ['palace', 'flat', 'house', 'bungalo'];
 
-    'offer': {
-      'type': 'palace'
-    },
+  for (var i = 1; i <= 8; i++) {
+    var dataItem = {};
 
-    'location': {
-      'x': 10,
-      'y': 130
-    }
-  },
-  {
-    'author': {
-      'avatar': 'img/avatars/user02.png'
-    },
+    dataItem.author = {'avatar': 'img/avatars/user0' + i + '.png'};
+    dataItem.offer = {'type': type[Math.floor(Math.random() * type.length)]};
+    dataItem.location = {'x': Math.floor(Math.random() * 1200), 'y': Math.floor(Math.random() * 131 + 500)};
 
-    'offer': {
-      'type': 'flat'
-    },
-
-    'location': {
-      'x': 20,
-      'y': 150
-    }
-  },
-  {
-    'author': {
-      'avatar': 'img/avatars/user03.png'
-    },
-
-    'offer': {
-      'type': 'house'
-    },
-
-    'location': {
-      'x': 30,
-      'y': 170
-    }
-  },
-  {
-    'author': {
-      'avatar': 'img/avatars/user04.png'
-    },
-
-    'offer': {
-      'type': 'bungalo'
-    },
-
-    'location': {
-      'x': 40,
-      'y': 190
-    }
-  },
-  {
-    'author': {
-      'avatar': 'img/avatars/user05.png'
-    },
-
-    'offer': {
-      'type': 'palace'
-    },
-
-    'location': {
-      'x': 50,
-      'y': 210
-    }
-  },
-  {
-    'author': {
-      'avatar': 'img/avatars/user06.png'
-    },
-
-    'offer': {
-      'type': 'flat'
-    },
-
-    'location': {
-      'x': 60,
-      'y': 230
-    }
-  },
-  {
-    'author': {
-      'avatar': 'img/avatars/user07.png'
-    },
-
-    'offer': {
-      'type': 'house'
-    },
-
-    'location': {
-      'x': 70,
-      'y': 250
-    }
-  },
-  {
-    'author': {
-      'avatar': 'img/avatars/user08.png'
-    },
-
-    'offer': {
-      'type': 'bungalo'
-    },
-
-    'location': {
-      'x': 80,
-      'y': 270
-    }
+    data.push(dataItem);
   }
-];
+  return data;
+};
 
 
-var pageMap = document.querySelector('.map');
-pageMap.classList.remove('map--faded');
+console.log(createMockData());
+
+/* var pageMap = document.querySelector('.map');
+pageMap.classList.remove('map--faded');*/
 
